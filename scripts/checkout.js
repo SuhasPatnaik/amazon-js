4026454dba84a3201d0d6250bc80d64cc7b5dfef
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-05-13 14:30:27 +0530

    Delete cart item from HTML feature added

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -99,5 +99,10 @@ deleteCartItem.forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const itemContainer = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    itemContainer.remove();
   });
 });
